618b71e9244e4e52c67e4d7c60e0a8b2b7a1561c
aslan <aslan.abdullayev.dev@gmail.com>
2024-04-27 19:29:10 +0400

    rust learning start

---
+++ courses/rust/target/debug/deps/rust.d
@@ -0,0 +1,5 @@
+C:\Users\User 2\Desktop\Learn-folder\courses\rust\target\debug\deps\rust.exe: src\main.rs
+
+C:\Users\User 2\Desktop\Learn-folder\courses\rust\target\debug\deps\rust.d: src\main.rs
+
+src\main.rs:
---
+++ courses/rust/target/debug/rust.d
@@ -0,0 +1,1 @@
+C:\Users\User\ 2\Desktop\Learn-folder\courses\rust\target\debug\rust.exe: C:\Users\User\ 2\Desktop\Learn-folder\courses\rust\src\main.rs
