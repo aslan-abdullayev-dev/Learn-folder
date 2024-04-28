155d0111e290a8ceaf10c8375c56a0cee65f7fdd
Aslan <aslan.abdullayev.dev@gmail.com>
2024-04-28 23:59:13 +0400

    update

--- courses/rust/playgroung/src/main.rs
+++ courses/rust/playgroung/src/main.rs
@@ -1,8 +1,13 @@
 fn main() {
+    let a = 32;
     println!("Hello, world!");
     print_hello();
+
+    fn print_hello() {
+        println!("Hello, rustacians, inside main")
+    }
 }
 
 fn print_hello() {
-    println!("Hello, rustacians")
+    println!("Hello, rustacians, outside main")
 }
