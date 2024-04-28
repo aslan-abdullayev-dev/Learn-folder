30674faa2a31a537ae38280325ffea27d7580147
Aslan <aslan.abdullayev.dev@gmail.com>
2024-04-28 23:57:02 +0400

    update

--- courses/dave_next_13_tutorial/next01/app/about/layout.tsx
+++ courses/dave_next_13_tutorial/next01/app/about/layout.tsx
@@ -3,14 +3,14 @@ import styles from "./styles.module.css"
 // * this layout applied only to the routes inside "app/about" directory
 
 export default function AboutLayout({
-  children,
-}: {
-  children: React.ReactNode
+                                        children,
+                                    }: {
+    children: React.ReactNode
 }) {
-  return (
-    <>
-      <nav>About Navbar</nav>
-      <main className={styles.main}>{children}</main>
-    </>
-  )
+    return (
+        <>
+            <nav>About Navbar</nav>
+            <main className={styles.main}>{children}</main>
+        </>
+    )
 }
