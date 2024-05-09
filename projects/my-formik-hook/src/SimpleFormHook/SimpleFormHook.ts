089b5b865d1b8836d46c92ae38f001adac7a2fc7
aslan <aslan.abdullayev.dev@gmail.com>
2024-05-09 13:40:23 +0400

    update

---
+++ projects/my-formik-hook/src/SimpleFormHook/SimpleFormHook.ts
@@ -0,0 +1,9 @@
+import { SimpleFormHookProps } from "./SimpleFormHookTypes.ts";
+
+export const SimpleFormHook = <T extends object>(props: SimpleFormHookProps<T>) => {
+  // console.log("initalValues ===>", initialValues)
+  console.log("props ===>", props)
+
+  return null
+}
+
---
+++ projects/my-formik-hook/src/SimpleFormHook/SimpleFormHookTypes.ts
@@ -0,0 +1,3 @@
+export interface SimpleFormHookProps<T> {
+  props: T
+}
