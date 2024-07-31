8356f484f2eaf58b00e64e60955430127f743a83
Aslan <aslan.abdullayev.dev@gmail.com>
2024-08-01 00:13:45 +0400

    update: delete redundant projects

--- courses/rust/playgroung/target/debug/deps/playgroung.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Aslan\Desktop\code\Learn-folder\courses\rust\playgroung\target\debug\deps\playgroung.exe: src\main.rs
-
-C:\Users\Aslan\Desktop\code\Learn-folder\courses\rust\playgroung\target\debug\deps\playgroung.d: src\main.rs
-
-src\main.rs:
--- courses/rust/playgroung/target/debug/playgroung.d
+++
@@ -1,1 +0,0 @@
-C:\Users\Aslan\Desktop\code\Learn-folder\courses\rust\playgroung\target\debug\playgroung.exe: C:\Users\Aslan\Desktop\code\Learn-folder\courses\rust\playgroung\src\main.rs
