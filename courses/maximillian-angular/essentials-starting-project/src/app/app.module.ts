5e3f4347211c25aeabbcea77929685a186d2f008
Aslan <aslan.abdullayev.dev@gmail.com>
2024-08-03 00:12:13 +0400

    feat: maximilian angular nested modules structure

--- courses/maximillian-angular/essentials-starting-project/src/app/app.module.ts
+++ courses/maximillian-angular/essentials-starting-project/src/app/app.module.ts
@@ -3,23 +3,16 @@ import { AppComponent } from "./app.component";
 import { BrowserModule } from "@angular/platform-browser";
 import { UserComponent } from "./user/user.component";
 import { HeaderComponent } from "./header/header.component";
-import { TasksComponent } from "./tasks/tasks.component";
-import { CardComponent } from "./shared/card/card.component";
-import { TaskComponent } from "./tasks/task/task.component";
-import { NewTaskComponent } from "./tasks/new-task/new-task.component";
-import { FormsModule } from "@angular/forms";
+import { SharedModule } from "./shared/shared.module";
+import { TasksModule } from "./tasks/tasks.module";
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     UserComponent,
-    TasksComponent,
-    TaskComponent,
-    NewTaskComponent,
-    CardComponent,
   ],
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, SharedModule, TasksModule],
   bootstrap: [AppComponent]
 })
 export class AppModule {
