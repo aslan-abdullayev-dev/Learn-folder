a9daad45b2fc3e7330250193175d833443833805
aslan <aslan.abdullayev.dev@gmail.com>
2024-08-02 13:09:28 +0400

    delete unnecessary folders

--- courses/maximillian-angular/essentials-starting-project/src/app/tasks/tasks.component.ts
+++ courses/maximillian-angular/essentials-starting-project/src/app/tasks/tasks.component.ts
@@ -2,18 +2,21 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { TaskComponent } from "./task/task.component";
 import { type TTask } from "./tasks.model";
 import { type TUser } from "../user/user.model";
+import { NewTaskComponent } from "../new-task/new-task.component";
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
   imports: [
-    TaskComponent
+    TaskComponent,
+    NewTaskComponent
   ],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
 export class TasksComponent {
   @Input({ required: true }) user!: TUser;
+  showNewTaskDialog: boolean = false;
 
   tasks: TTask[] = [
     {
@@ -47,5 +50,9 @@ export class TasksComponent {
 
   completeTheTask(id: TTask["id"]) {
     this.tasks = this.tasks.filter(t => t.id !== id);
+  }
+
+  openNewTaskDialog(): void {
+    this.showNewTaskDialog = true;
   }
 }
