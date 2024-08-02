62bf498e61ac9560c74c5f378fc7c879205df5ad
aslan <aslan.abdullayev.dev@gmail.com>
2024-08-02 16:28:35 +0400

    feat: maximilian angular date pipe

--- courses/maximillian-angular/essentials-starting-project/src/app/tasks/task/task.component.ts
+++ courses/maximillian-angular/essentials-starting-project/src/app/tasks/task/task.component.ts
@@ -1,13 +1,15 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { TTask } from "../tasks.model";
 import { CardComponent } from "../../shared/card/card.component";
+import { DatePipe } from "@angular/common";
 
 @Component({
   selector: 'app-task',
   standalone: true,
   templateUrl: './task.component.html',
   imports: [
-    CardComponent
+    CardComponent,
+    DatePipe
   ],
   styleUrl: './task.component.css'
 })
