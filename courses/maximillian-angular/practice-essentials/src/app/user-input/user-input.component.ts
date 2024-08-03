295f700db957ac168f1e99111a9b6c915a6b650f
Aslan <aslan.abdullayev.dev@gmail.com>
2024-08-03 14:53:04 +0400

    feat: maximilian angular migrating components to modules

--- courses/maximillian-angular/practice-essentials/src/app/user-input/user-input.component.ts
+++ courses/maximillian-angular/practice-essentials/src/app/user-input/user-input.component.ts
@@ -1,11 +1,8 @@
 import { Component, signal, inject } from '@angular/core';
-import { FormsModule } from "@angular/forms";
 import { InvestmentResultsService } from "../investment-results/investment-results.service";
 
 @Component({
   selector: 'app-user-input',
-  standalone: true,
-  imports: [FormsModule],
   templateUrl: './user-input.component.html',
   styleUrl: './user-input.component.css'
 })
