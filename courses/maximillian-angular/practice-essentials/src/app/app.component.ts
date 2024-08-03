fbea36b1de3c109b80a693b1b98a60452b40acf8
Aslan <aslan.abdullayev.dev@gmail.com>
2024-08-03 12:12:52 +0400

    feat: maximilian angular calculate investment

--- courses/maximillian-angular/practice-essentials/src/app/app.component.ts
+++ courses/maximillian-angular/practice-essentials/src/app/app.component.ts
@@ -39,7 +39,7 @@ export class AppComponent {
         totalAmountInvested: initialInvestment + annualInvestment * year,
       });
     }
-
+    console.log(annualData)
     return annualData;
   }
 }
--- courses/maximillian-angular/practice-essentials/src/app/user-input/user-input.component.ts
+++ courses/maximillian-angular/practice-essentials/src/app/user-input/user-input.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 import { FormsModule } from "@angular/forms";
+import { TInvestmentData } from "./user-input.model";
 
 @Component({
   selector: 'app-user-input',
@@ -9,12 +10,19 @@ import { FormsModule } from "@angular/forms";
   styleUrl: './user-input.component.css'
 })
 export class UserInputComponent {
+  @Output() onCalculate = new EventEmitter<TInvestmentData>();
+
   initialInvestment = "0";
   annualInvestment = "0";
   expectedReturn = "0";
   duration = "0";
 
   handleCalculate() {
-    console.log(this)
+    this.onCalculate.emit({
+      initialInvestment: Number(this.initialInvestment),
+      annualInvestment: Number(this.initialInvestment),
+      expectedReturn: Number(this.initialInvestment),
+      duration: Number(this.initialInvestment),
+    });
   }
 }
