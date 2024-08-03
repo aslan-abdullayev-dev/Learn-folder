ce587956829caa2c6c48caeae07d14b98c2cd671
Aslan <aslan.abdullayev.dev@gmail.com>
2024-08-03 11:05:39 +0400

    feat: maximilian angular calculate investment results formula

--- courses/maximillian-angular/practice-essentials/src/app/app.component.ts
+++ courses/maximillian-angular/practice-essentials/src/app/app.component.ts
@@ -1,8 +1,45 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "./header/header.component";
+import { UserInputComponent } from "./user-input/user-input.component";
+import { TInvestmentData } from "./user-input/user-input.model";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
+  imports: [
+    HeaderComponent,
+    UserInputComponent
+  ]
 })
-export class AppComponent {}
+export class AppComponent {
+  calculateInvestmentResults(data: TInvestmentData) {
+    const {
+      initialInvestment,
+      annualInvestment,
+      expectedReturn,
+      duration
+    } = data
+
+    const annualData = [];
+    let investmentValue = initialInvestment;
+
+    for (let i = 0; i < duration; i++) {
+      const year = i + 1;
+      const interestEarnedInYear = investmentValue * (expectedReturn / 100);
+      investmentValue += interestEarnedInYear + annualInvestment;
+      const totalInterest =
+        investmentValue - annualInvestment * year - initialInvestment;
+      annualData.push({
+        year: year,
+        interest: interestEarnedInYear,
+        valueEndOfYear: investmentValue,
+        annualInvestment: annualInvestment,
+        totalInterest: totalInterest,
+        totalAmountInvested: initialInvestment + annualInvestment * year,
+      });
+    }
+
+    return annualData;
+  }
+}
---
+++ courses/maximillian-angular/practice-essentials/src/app/user-input/user-input.model.ts
@@ -0,0 +1,6 @@
+export type TInvestmentData = {
+  initialInvestment: number,
+  duration: number,
+  expectedReturn: number,
+  annualInvestment: number
+}
