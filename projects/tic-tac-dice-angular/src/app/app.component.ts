2a72c381edd90d1c7c75bc327191d3e6bb13cd21
aslan <aslan.abdullayev.dev@gmail.com>
2024-08-05 16:57:07 +0400

    feat: tic-tac-dice-angular first components

--- projects/tic-tac-dice-angular/src/app/app.component.ts
+++ projects/tic-tac-dice-angular/src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./header/header.component";
+import { LeftSidebarComponent } from "./left-sidebar/left-sidebar.component";
+import { RightSidebarComponent } from "./right-sidebar/right-sidebar.component";
+import { BoardComponent } from "./board/board.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, LeftSidebarComponent, RightSidebarComponent, BoardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'tic-tac-dice-angular';
 }
