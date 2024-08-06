fa24d05a089d2bab0f21961fac98ae73c8609e9f
aslan <aslan.abdullayev.dev@gmail.com>
2024-08-06 17:14:31 +0400

    feat: tic-tac-dice-angular roll dice component created

---
+++ projects/tic-tac-dice-angular/src/app/roll-dice-btn/roll-dice-btn.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ttd-roll-dice-btn',
+  standalone: true,
+  imports: [],
+  templateUrl: './roll-dice-btn.component.html',
+  styleUrl: './roll-dice-btn.component.scss'
+})
+export class RollDiceBtnComponent {
+
+}
