2dd6d3ea7d4b4f15e6330aef439077765bd671a6
aslan <aslan.abdullayev.dev@gmail.com>
2024-08-19 18:54:54 +0400

    maximilian-angular: tickets removing template variables adding ngmodule

--- courses/maximillian-angular/components-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ courses/maximillian-angular/components-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { ControlComponent } from "../../../shared/control/control.component";
 import { ButtonComponent } from "../../../shared/button/button.component";
 import { FormsModule } from "@angular/forms";
@@ -15,11 +15,13 @@ import { FormsModule } from "@angular/forms";
   styleUrl: './new-ticket.component.css'
 })
 export class NewTicketComponent {
-  @ViewChild("form") private form?: ElementRef<HTMLFormElement>;
   @Output() public add = new EventEmitter<{ title: string; text: string }>();
+  title = "";
+  request = "";
 
-  handleSubmit(title: string, text: string) {
-    this.add.emit({ title, text })
-    this.form?.nativeElement.reset()
+  handleSubmit() {
+    this.add.emit({ title: this.title, text: this.request })
+    this.title = ""
+    this.request = ""
   }
 }
