cab6f97746762fdd595d69b1809c8ae82aa4421e
aslan <aslan.abdullayev.dev@gmail.com>
2024-08-27 19:50:07 +0400

    maximilian-angular-2: finish 72

--- courses/maximillian-angular/01-starting-project/src/app/app.module.ts
+++ courses/maximillian-angular/01-starting-project/src/app/app.module.ts
@@ -1,29 +1,23 @@
 import { BrowserModule } from "@angular/platform-browser";
-import { FormsModule } from "@angular/forms";
 import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
+import { SharedModule } from "./shared/shared.module";
 import { UserComponent } from "./user/user.component";
-import { TasksComponent } from "./tasks/tasks.component";
-import { TaskComponent } from "./tasks/task/task.component";
 import { HeaderComponent } from "./header/header.component";
-import { CardComponent } from "./shared/card/card.component";
-import { NewTaskComponent } from "./tasks/new-task/new-task.component";
+import { TasksModule } from "./tasks/tasks.module";
 
 @NgModule({
     declarations: [
         AppComponent,
         HeaderComponent,
         UserComponent,
-        CardComponent, //* used inside UserComponent
-        TasksComponent,
-        TaskComponent, //* used inside TasksComponent
-        NewTaskComponent //* used inside TasksComponent
     ], //* register all the components and directives
     bootstrap: [AppComponent], //* start application bootstrap from this component
     imports: [
         BrowserModule, //* brings angular common module
-        FormsModule
+        SharedModule,
+        TasksModule
     ] //* register standalone components and other modules here
 })
 export class AppModule {
