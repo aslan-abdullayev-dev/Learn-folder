52c779f472cc36442d5f2b3925f2ea1bedbc8f38
Aslan <aslan.abdullayev.dev@gmail.com>
2024-09-06 13:18:23 +0400

    maximilian-nextjs: finished 100

---
+++ courses/maximilian-nextjs/foodies-roject/app/main-header.js
@@ -0,0 +1,30 @@
+import classes from './main-header.module.css';
+
+import Link from "next/link";
+
+import logoImg from "@/assets/logo.png";
+
+
+const MainHeader = () => {
+  return (
+    <header className={classes.header}>
+      <Link className={classes.logo} href="/">
+        <img src={logoImg.src} alt="A plate with food on it"/>
+        NextLevel Food
+      </Link>
+
+      <nav className={classes.nav}>
+        <ul>
+          <li>
+            <Link href="/meals">Browse Meals</Link>
+          </li>
+          <li>
+            <Link href="/community">Browse Meals</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
+
+export default MainHeader;
