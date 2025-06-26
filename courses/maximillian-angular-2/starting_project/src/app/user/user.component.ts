821bf6557423dc2f94d8d7bb1527511d3c6e7f0f
Aslan <aslan.abdullayev.dev@gmail.com>
2025-06-27 03:04:32 +0400

    maximillian-angular-2: starting project created tasks component

--- courses/maximillian-angular-2/starting_project/src/app/user/user.component.ts
+++ courses/maximillian-angular-2/starting_project/src/app/user/user.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
-import { DUMMY_USERS } from "../dummy-users";
-
-const randomIndex = () => Math.floor(Math.random() * DUMMY_USERS.length);
+interface User {
+  id: string;
+  name: string;
+  avatar: string
+}
 
 @Component({
   selector: 'app-user',
@@ -13,13 +15,15 @@ const randomIndex = () => Math.floor(Math.random() * DUMMY_USERS.length);
 })
 
 export class UserComponent {
-  selectedUser = DUMMY_USERS[randomIndex()]
+  @Input({required: true}) user!: User
+
+  @Output() select = new EventEmitter<string>()
 
   get imagePath() {
-    return 'assets/users/users/' + this.selectedUser.avatar
+    return `assets/users/${this.user.avatar}`
   }
 
   onSelectUser() {
-    this.selectedUser = DUMMY_USERS[randomIndex()]
+    this.select.emit(this.user.id)
   }
 }
