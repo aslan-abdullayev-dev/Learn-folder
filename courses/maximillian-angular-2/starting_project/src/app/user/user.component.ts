cc79fa9eff146ac562bc0d8fd93e4a37df2f2732
Aslan <aslan.abdullayev.dev@gmail.com>
2025-06-27 00:26:18 +0400

    maximillian-angular-2: starting project added user click functionality

--- courses/maximillian-angular-2/starting_project/src/app/user/user.component.ts
+++ courses/maximillian-angular-2/starting_project/src/app/user/user.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 
+import { DUMMY_USERS } from "../dummy-users";
+
+const randomIndex = () => Math.floor(Math.random() * DUMMY_USERS.length);
+
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -7,6 +11,15 @@ import { Component } from '@angular/core';
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
-export class UserComponent {
 
+export class UserComponent {
+  selectedUser = DUMMY_USERS[randomIndex()]
+
+  get imagePath() {
+    return 'assets/users/users/' + this.selectedUser.avatar
+  }
+
+  onSelectUser() {
+    this.selectedUser = DUMMY_USERS[randomIndex()]
+  }
 }
