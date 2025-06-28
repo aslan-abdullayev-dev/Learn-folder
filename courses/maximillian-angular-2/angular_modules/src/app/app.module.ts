5a0215595cc65af1a91dd5d48cd4ebba9c965565
Aslan <aslan.abdullayev.dev@gmail.com>
2025-06-28 13:51:53 +0400

    maximillian-angular-2: angular modules remoced all standalone components

--- courses/maximillian-angular-2/angular_modules/src/app/app.module.ts
+++ courses/maximillian-angular-2/angular_modules/src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 import { BrowserModule } from "@angular/platform-browser";
 
 import { AppComponent } from "./app.component";
 import { UserComponent } from "./user/user.component";
 import { TasksComponent } from "./tasks/tasks.component";
+import { TaskComponent } from "./tasks/task/task.component";
 import { HeaderComponent } from "./header/header.component";
+import { CardComponent } from "./shared/card/card.component";
+import { NewTaskComponent } from "./tasks/new-task/new-task.component";
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    UserComponent,
+    TasksComponent,
+    CardComponent,
+    TaskComponent,
+    NewTaskComponent
+  ],
   bootstrap: [AppComponent],
-  imports: [BrowserModule, HeaderComponent, UserComponent, TasksComponent],
-
+  imports: [BrowserModule, FormsModule],
 })
 export class AppModule {
 }
