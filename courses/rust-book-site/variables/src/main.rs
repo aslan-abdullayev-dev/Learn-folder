245566a12fc63ebea02aeedb1614028d23dd5e59
Aslan <aslan.abdullayev.dev@gmail.com>
2026-04-08 21:14:21 +0400

    Feat(rust-book-site) removed learning directory to start from scratch with ai

--- courses/rust-book-site/variables/src/main.rs
+++
@@ -1,9 +0,0 @@
-pub fn is_palindrome(s: String) -> bool {
-    
-    true
-}
-
-fn main() {
-    let s = "A man, a plan, a canal: Panama".to_string();
-    println!("res ==> {:?}", is_palindrome(s));
-}
